62798f31097016a9519f498bd072ff55c4e78722
Lauriuche <lauriuche@gmail.com>
2025-11-25 19:26:33 -0300

    Delete enviar-telegram.php

--- enviar-telegram.php
+++
@@ -1,6 +0,0 @@
-<?php
-require 'config.php';
-$msg = $_GET['msg'] ?? 'Teste de envio';
-file_get_contents("https://api.telegram.org/bot".BOT_TOKEN."/sendMessage?chat_id=".CHAT_ID."&text=".urlencode($msg));
-echo "Enviado!";
-?>
